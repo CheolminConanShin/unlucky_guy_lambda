1356ff462d2e5c5ff33967c3f6b359e6e3ace480
CheolminConanShin <scm0226@gmail.com>
2017-07-08 22:13:26 +0900

    GOAL: Ranking information getter lambda function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,19 @@
+var documentClient = new AWS.DynamoDB.DocumentClient()
+
 exports.handler = (event, context, callback) => {
-    callback(null, {
-        statusCode: '200'
-    });
+    var params = {
+        TableName : 'unlucky-guy',
+    };
+
+
+    documentClient.scan(params, function (err, data) {
+        var response = {
+            headers: {
+                statusCode: '200'
+            },
+            body: data
+        }
+
+        callback(null, response)
+    })
 };
